d465bdd47792aa8578cd06dc9f0a06fdc5d34558
toyobayashi <lifenglin314@outlook.com>
2021-05-29 18:50:09 +0800

    lame

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+localprefix="$(pwd)/.local"
+
+cd ./deps/lame
+emconfigure ./configure --enable-shared=no --enable-nasm=no --prefix="$localprefix"
+emmake make
+emmake make install
+cd ../..
+cp -rpf "$localprefix/*" "$EMSDK/upstream/emscripten/system/local"
+
 tag="n4.4"
 curl -O -L "https://github.com/FFmpeg/FFmpeg/archive/refs/tags/$tag.tar.gz"
 tar xf "$tag.tar.gz"
@@ -15,14 +24,15 @@ emconfigure ./configure --disable-programs \
   --disable-pthreads \
   --disable-network \
   --disable-debug \
+  --enable-libmp3lame \
   --cc=emcc \
   --cxx=em++ \
   --ar=emar \
   --ranlib=emranlib \
-  --prefix=./.local
+  --prefix="$localprefix"
 
 emmake make
 emmake make install
-cp -rpf ./.local ../dist
-cp -rpf ./.local/* "$EMSDK/upstream/emscripten/system/local"
+cp -rpf "$localprefix" ../dist
+cp -rpf "$localprefix/*" "$EMSDK/upstream/emscripten/system/local"
 cd ..
